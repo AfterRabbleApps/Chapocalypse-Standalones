7187ca8f8f5462a277cc89bfbcfd68286f7a50f3
Andrew Chumchal <andyipod1437@gmail.com>
2016-11-26 19:50:34 -0600

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -32,6 +32,4 @@ install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller//UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-$VERSION.pkg"
+
